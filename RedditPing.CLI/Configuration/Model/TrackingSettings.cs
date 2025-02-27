6d9e32587abdb4c92dec7d87b0b257d471330fdf
Angela <angela.stevkovska3@gmail.com>
2025-02-27 23:49:05 +0100

    Change type on tracking settings - subreddits

--- RedditPing.CLI/Configuration/Model/TrackingSettings.cs
+++ RedditPing.CLI/Configuration/Model/TrackingSettings.cs
@@ -3,6 +3,7 @@ namespace RedditPing.CLI.Configuration.Model
 {
     public class TrackingSettings
     {
-        public List<string>? Subreddits { get; set; }
+        //public List<string>? Subreddits { get; set; }
+        public string? Subreddits { get; set; }
     }
 }
